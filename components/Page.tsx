e8c89df8574487c5572dd17382fdff64bd558548
Renzo <renzol2@illinois.edu>
2021-12-31 22:55:39 -0600

    Add padding

--- components/Page.tsx
+++ components/Page.tsx
@@ -18,7 +18,12 @@ export default function Page({
         <meta name="description" content="i write code and music :)" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Grid templateColumns={{ base: '100%', md: '20% 80%' }} p="2.5%">
+      <Grid
+        templateColumns={{ base: '100%', md: '20% 80%' }}
+        px={{ base: '7%', md: '2.5%' }}
+        pt={{ base: '6%', md: '3%' }}
+        pb="7rem"
+      >
         <Navbar />
         <Box color={TEXT}>{children}</Box>
       </Grid>
