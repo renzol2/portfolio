27afe4a0280d323a6e260e332292aab4bd11aa61
Renzo <renzol2@illinois.edu>
2022-05-21 17:07:39 -0500

    Finish self description

--- theme/colors.ts
+++ theme/colors.ts
@@ -8,6 +8,9 @@ export const INSTAGRAM = '#A70046';
 export const URBANA_ORANGE = '#E84A27';
 export const CARGILL_GREEN = '#008544';
 export const PRAIRIELEARN_BLUE = '#007BFF';
+export const COURSERA_BLUE = '#0056D2';
+export const PAYLOCITY_ORANGE = '#FD6413';
+export const PSA_MAROON = '#930000';
 export const RANDOM_COLORS = [
   HIGHLIGHT_COLOR,
   PRIMARY_ACCENT,
