8e4096cf1f40b483470b3110af16c5d7ae3bc202
Renzo <renzol2@illinois.edu>
2022-05-21 19:00:43 -0500

    Add more info

--- pages/kapit-bisig.tsx
+++ pages/kapit-bisig.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Image from 'next/image';
 import mdxComponents from '../components/mdxComponents';
@@ -8,15 +9,17 @@ const KapitBisig: NextPage = () => {
   const IMAGE_SOURCE = '/images/kapit_bisig.png';
   return (
     <Page>
-      <Image
-        src={IMAGE_SOURCE}
-        alt="Kapit Bisig"
-        width={1896}
-        height={1080}
-        placeholder="blur"
-        blurDataURL={IMAGE_SOURCE}
-      />
-      <KapitBisigContent components={mdxComponents} />
+      <Box w="100%">
+        <Image
+          src={IMAGE_SOURCE}
+          alt="Kapit Bisig"
+          width={1896}
+          height={1080}
+          placeholder="blur"
+          blurDataURL={IMAGE_SOURCE}
+        />
+        <KapitBisigContent components={mdxComponents} />
+      </Box>
     </Page>
   );
 };
